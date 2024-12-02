68cb9c02b9370649ac55de1eafee3dac5797052c
kk <kzou227@qq.com>
2024-12-02 19:48:02 +0800

    新增单元测试

--- mahu-main/mahu-main-infra/src/test/java/cool/houge/mahu/repository/UserRepositoryTest.java
+++ mahu-main/mahu-main-infra/src/test/java/cool/houge/mahu/repository/UserRepositoryTest.java
@@ -1,8 +1,15 @@
 package cool.houge.mahu.repository;
 
 import cool.houge.mahu.TestTransactionBase;
+import cool.houge.mahu.entity.User;
+import io.ebean.annotation.SoftDelete;
 import jakarta.inject.Inject;
+import jakarta.persistence.Id;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.Select.fields;
 
 /// 用户
 ///
@@ -14,18 +21,18 @@ class UserRepositoryTest extends TestTransactionBase {
 
     @Test
     void findByWechatAppid$Openid() {
-//        var entity = Instancio.of(User.class)
-//            .ignore(fields().annotated(Id.class))
-//            .supply(fields().annotated(SoftDelete.class), () -> false)
-//            .create();
-//
-//        userRepository.save(entity);
-//
-//        var wechatProfile = entity.getWechatProfile().setId(entity.getId());
-//        getDatabase().save(wechatProfile);
+        var entity = Instancio.of(User.class)
+                .ignore(fields().annotated(Id.class))
+                .supply(fields().annotated(SoftDelete.class), () -> false)
+                .create();
+        userRepository.save(entity);
 
-        var dbEntity = userRepository.findByWechatAppid$Openid("UHTLEANSUQ", "QRVS");
-        System.out.println(dbEntity);
-        System.out.println(dbEntity.getWechatProfile().getAppid());
+        var dbEntity = userRepository.findByWechatAppid$Openid(
+                entity.getWechatProfile().getAppid(), entity.getWechatProfile().getOpenid());
+        assertThat(dbEntity)
+                .isNotNull()
+                .usingRecursiveComparison()
+                .ignoringFields("createTime", "updateTime")
+                .isEqualTo(entity);
     }
 }
