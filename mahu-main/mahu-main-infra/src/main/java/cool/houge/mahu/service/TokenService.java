50e787815cf21ff5c02c0c1a139b11e1d4d786cf
kk <kzou227@qq.com>
2024-12-03 21:50:11 +0800

    u

--- mahu-main/mahu-main-infra/src/main/java/cool/houge/mahu/service/TokenService.java
+++ mahu-main/mahu-main-infra/src/main/java/cool/houge/mahu/service/TokenService.java
@@ -1,22 +1,33 @@
 package cool.houge.mahu.service;
 
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
 import com.google.common.base.Strings;
 import cool.houge.mahu.common.GrantType;
 import cool.houge.mahu.common.security.AuthContext;
 import cool.houge.mahu.common.security.TokenVerifier;
+import cool.houge.mahu.config.TokenConfig;
 import cool.houge.mahu.entity.User;
 import cool.houge.mahu.entity.WechatProfile;
+import cool.houge.mahu.entity.system.Client;
 import cool.houge.mahu.remote.wechat.Jscode2SessionPayload;
 import cool.houge.mahu.remote.wechat.WechatClient;
 import cool.houge.mahu.remote.wechat.WechatEncryptPayload;
 import cool.houge.mahu.repository.UserRepository;
 import cool.houge.mahu.system.repository.ClientRepository;
 import io.ebean.annotation.Transactional;
+import io.helidon.security.jwt.EncryptedJwt;
+import io.helidon.security.jwt.Jwt;
+import io.helidon.security.jwt.SignedJwt;
+import io.helidon.security.jwt.jwk.Jwk;
+import io.helidon.security.jwt.jwk.JwkKeys;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.Instant;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 
 /// 令牌
@@ -26,6 +37,12 @@ import java.util.function.Supplier;
 public class TokenService implements TokenVerifier {
 
     private static final Logger log = LogManager.getLogger(TokenService.class);
+
+    @Inject
+    JwkKeys jwkKeys;
+
+    @Inject
+    TokenConfig tokenConfig;
 
     @Inject
     ClientRepository clientRepository;
@@ -50,6 +67,42 @@ public class TokenService implements TokenVerifier {
 
     void loginByRefreshToken(TokenPayload payload) {
         //
+    }
+
+    void makeToken(Client client, User user) {
+        var jwk = obtainJwk();
+        var jwtId = UlidCreator.getUlid().toLowerCase();
+        var sub = String.valueOf(user.getId());
+        var iat = Instant.now();
+        var atJwt = Jwt.builder()
+                .jwtId(jwtId)
+                .subject(sub)
+                .issueTime(iat)
+                .expirationTime(iat.plus(tokenConfig.accessExpires()))
+                .addAudience(client.getClientId())
+                .nonce(Ulid.fast().toLowerCase())
+                .build();
+        var accessToken = EncryptedJwt.builder(SignedJwt.sign(atJwt, Jwk.NONE_JWK))
+                .jwks(jwkKeys, jwk.keyId())
+                .build();
+
+        var rtJwt = Jwt.builder()
+                .jwtId(jwtId)
+                .subject(sub)
+                .issueTime(iat)
+                .expirationTime(iat.plus(tokenConfig.refreshExpires()))
+                .nonce(Ulid.fast().toLowerCase())
+                .build();
+        var refreshToken = EncryptedJwt.builder(SignedJwt.sign(rtJwt, Jwk.NONE_JWK))
+                .jwks(jwkKeys, jwk.keyId())
+                .build();
+    }
+
+    Jwk obtainJwk() {
+        var keys = jwkKeys.keys();
+        var ran = ThreadLocalRandom.current();
+        var i = ran.nextInt(keys.size());
+        return keys.get(i);
     }
 
     User loginByWechatXcx(TokenPayload payload) {
