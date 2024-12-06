1c9bdfd25f01706c4fbef3b21c4a613d116460cd
ZY <kzou227@qq.com>
2024-12-06 18:44:20 +0800

    修改 token_jour 表结构

--- mahu-db/changelog/changelog-tables/system.token_jour.sql
+++ mahu-db/changelog/changelog-tables/system.token_jour.sql
@@ -4,27 +4,28 @@
 -- changeset kzou227@qq.com:202403211334
 create table system.token_jour
 (
-    id            character varying(32) primary key not null,
-    create_time   timestamp without time zone, -- 创建时间
-    type          character varying(16),       -- 令牌类型
-    sub           character varying(32),       -- 用户唯一名
-    client_id     character varying(32),       -- 客户端ID
-    client_ip     character varying(64),       -- 客户端IP
-    jwk_id        character varying(32),       -- JWT密钥ID
-    grant_type    character varying(32),       -- 授权类型
-    access_token  character varying(2048),     -- 访问令牌
-    refresh_token character varying(2048)      -- 刷新令牌
+    id          character varying(32) primary key not null,
+    create_time timestamp without time zone, -- 创建时间
+    upn         character varying(32),       -- 用户主体
+    client_id   character varying(32),       -- 客户端ID
+    client_addr character varying(64),       -- 客户端IP
+    jwk_id      character varying(32),       -- JWT密钥ID
+    grant_type  character varying(32)        -- 授权类型
 );
-comment on table system.token_jour is '访问令牌日志';
-comment on column system.token_jour.create_time is '创建时间';
-comment on column system.token_jour.type is '令牌类型';
-comment on column system.token_jour.sub is '用户唯一名';
-comment on column system.token_jour.client_id is '客户端ID';
-comment on column system.token_jour.client_ip is '客户端IP';
-comment on column system.token_jour.jwk_id is 'JWT密钥ID';
-comment on column system.token_jour.grant_type is '授权类型';
-comment on column system.token_jour.access_token is '访问令牌';
-comment on column system.token_jour.refresh_token is '刷新令牌';
+comment
+on table system.token_jour is '访问令牌日志';
+comment
+on column system.token_jour.create_time is '创建时间';
+comment
+on column system.token_jour.upn is '用户主体';
+comment
+on column system.token_jour.client_id is '客户端ID';
+comment
+on column system.token_jour.client_addr is '客户端IP';
+comment
+on column system.token_jour.jwk_id is 'JWT密钥ID';
+comment
+on column system.token_jour.grant_type is '授权类型';
 
-create index token_jour_sub_idx on system.token_jour using btree (sub);
+create index token_jour_sub_idx on token_jour using btree (upn);
 -- rollback drop table system.token_jour;
