539b134a7416e44a0ecce2b826e8a8c7fe620cd7
kk <kzou227@qq.com>
2024-12-15 19:05:35 +0800

    重构认证模块

---
+++ mahu-common/src/main/java/cool/houge/mahu/common/web/ValidatorHolder.java
@@ -0,0 +1,12 @@
+package cool.houge.mahu.common.web;
+
+import io.avaje.validation.Validator;
+
+/// 数据校器验持有对象
+///
+/// @author ZY (kzou227@qq.com)
+class ValidatorHolder {
+
+    /// 数据校验器
+    static final Validator VALIDATOR = Validator.builder().build();
+}
--- mahu-common/src/main/java/cool/houge/mahu/common/web/WebSupport.java
+++ mahu-common/src/main/java/cool/houge/mahu/common/web/WebSupport.java
@@ -1,18 +1,10 @@
 package cool.houge.mahu.common.web;
 
 import com.google.common.base.Splitter;
-import cool.houge.lang.BizCodeException;
-import cool.houge.lang.BizCodes;
 import cool.houge.mahu.common.DataFilter;
-import cool.houge.mahu.common.security.AuthContext;
-import io.avaje.validation.Validator;
-import io.helidon.common.LazyValue;
-import io.helidon.common.context.Contexts;
 import io.helidon.http.HeaderNames;
 import io.helidon.webserver.http.SecureHandler;
 import io.helidon.webserver.http.ServerRequest;
-
-import javax.annotation.Nonnull;
 
 import static io.helidon.webserver.http.SecureHandler.authenticate;
 
@@ -21,14 +13,11 @@ import static io.helidon.webserver.http.SecureHandler.authenticate;
 /// @author ZY (kzou227@qq.com)
 public interface WebSupport {
 
-    LazyValue<Validator> VALIDATOR_LV =
-        LazyValue.create(() -> Validator.builder().build());
-
     /// 校验对象
     ///
     /// @param o 校验的对象
     default void validate(Object o) {
-        VALIDATOR_LV.get().validate(o);
+        ValidatorHolder.VALIDATOR.validate(o);
     }
 
     /// 获取数据过滤对象
@@ -41,15 +30,15 @@ public interface WebSupport {
     /// 获取客户端请求 IP
     ///
     /// @param request 请求对象
-    default String clientIp(ServerRequest request) {
+    default String clientAddr(ServerRequest request) {
         return request.headers()
-            .first(HeaderNames.X_FORWARDED_FOR)
-            .map(s -> {
-                var splitter = Splitter.on(',').omitEmptyStrings().trimResults();
-                var ipList = splitter.splitToList(s);
-                return ipList.getFirst();
-            })
-            .orElseGet(() -> request.remotePeer().host());
+                .first(HeaderNames.X_FORWARDED_FOR)
+                .map(s -> {
+                    var splitter = Splitter.on(',').omitEmptyStrings().trimResults();
+                    var ipList = splitter.splitToList(s);
+                    return ipList.getFirst();
+                })
+                .orElseGet(() -> request.remotePeer().host());
     }
 
     /// @param permits 权限名称
@@ -58,19 +47,5 @@ public interface WebSupport {
             return authenticate();
         }
         return authenticate().andAuthorize(permits);
-    }
-
-    /// 获取认证上下文
-    default @Nonnull AuthContext authContext() {
-        return Contexts.context()
-            .orElseThrow(() -> new BizCodeException(BizCodes.UNAVAILABLE, "没有发现 helidon 上下文对象"))
-            .get(AuthContext.class)
-            .orElseThrow(() -> new BizCodeException(BizCodes.UNAUTHENTICATED, "缺少认证上下文"));
-    }
-
-    /// 当前认证的用户 ID
-    default long uid() {
-        var ac = authContext();
-        return ac.uid();
     }
 }
