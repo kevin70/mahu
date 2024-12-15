aa5773e050c1d5e81ff927882603868dfd674ff3
kk <kzou227@qq.com>
2024-12-15 23:20:01 +0800

    新增商店资源

---
+++ mahu-entity/src/main/java/cool/houge/mahu/entity/market/Asset.java
@@ -0,0 +1,30 @@
+package cool.houge.mahu.entity.market;
+
+import io.ebean.annotation.WhenCreated;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+///
+/// @author ZY (kzou227@qq.com)
+@Getter
+@Setter
+@Entity
+@Table(name = "asset", schema = "market")
+public class Asset {
+
+    /// 主键
+    @Id
+    @GeneratedValue
+    private Long id;
+    /// 创建时间
+    @WhenCreated
+    private Instant createTime;
+    /// 资源 URI
+    private String uri;
+    /// 所属商店
+    @ManyToOne
+    private Shop shop;
+}
