7f9cb00105510f8cdace5346ebb33979afda2a9c
ZY <kzou227@qq.com>
2024-12-17 18:08:01 +0800

    完善测试

---
+++ mahu-admin/mahu-admin-infra/src/test/java/cool/houge/mahu/admin/market/repository/AssetRepositoryTest.java
@@ -0,0 +1,36 @@
+package cool.houge.mahu.admin.market.repository;
+
+import cool.houge.mahu.admin.TestTransactionBase;
+import cool.houge.mahu.entity.market.Asset;
+import cool.houge.mahu.entity.market.Shop;
+import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.WhenModified;
+import jakarta.inject.Inject;
+import jakarta.persistence.Id;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import static org.instancio.Select.field;
+import static org.instancio.Select.fields;
+
+///
+/// @author ZY (kzou227@qq.com)
+class AssetRepositoryTest extends TestTransactionBase {
+
+    @Inject
+    AssetRepository assetRepository;
+
+    @Test
+    void save() {
+        var entities = Instancio.of(Asset.class)
+            .ignore(fields().annotated(Id.class))
+            .ignore(fields().annotated(WhenCreated.class))
+            .ignore(fields().annotated(WhenModified.class))
+            .supply(field(Asset::getShop), ()-> new Shop().setId(2))
+            .supply(field(Asset::getUri), ()-> "https://placehold.co/600x400")
+            .stream()
+            .limit(199)
+            .toList();
+        assetRepository.saveAll(entities);
+    }
+}
