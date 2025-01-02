bf0d23257ab79cc7fb2a414a10b89e91246c6f1f
kk <kzou227@qq.com>
2025-01-02 22:35:10 +0800

    U

--- mahu-ui/mahu-admin-ui/src/hooks/useDataFilter.ts
+++ mahu-ui/mahu-admin-ui/src/hooks/useDataFilter.ts
@@ -4,6 +4,57 @@ import { ComparisonNode, ExpressionNode } from '@rsql/ast';
 import { emit } from '@rsql/emitter';
 
 /**
+ * RSQL 操作
+ */
+export const rsqlOps = {
+  ...rsqlBuilder,
+  contains(selector: string, value: string) {
+    return rsqlBuilder.comparison(selector, '=contains=', value);
+  },
+  icontains(selector: string, value: string) {
+    return rsqlBuilder.comparison(selector, '=icontains=', value);
+  },
+  between(selector: string, values: (string | number)[]) {
+    if (values.length != 2) {
+      throw new Error('between 操作符数据必须有两个值');
+    }
+    return rsqlBuilder.comparison(selector, '=between=', values);
+  },
+  comparisonEx(
+    selector: string,
+    operator:
+      | '=='
+      | '!='
+      | '<='
+      | '>='
+      | '<'
+      | '>'
+      | '=in='
+      | '=out='
+      | '=le='
+      | '=ge='
+      | '=lt='
+      | '=gt='
+      | '=contains='
+      | '=icontains=',
+    value: string | number | (string | number)[] | undefined | null
+  ): ComparisonNode | null {
+    if (typeof value === 'undefined' || value === null || value === '') {
+      return null;
+    }
+    if (Array.isArray(value) && value.length <= 0) {
+      return null;
+    }
+    return rsqlBuilder.comparison(selector, operator, value);
+  },
+  // RSQL 查询编码
+  encode(nodes: ExpressionNode[]) {
+    const node = rsqlBuilder.logic(nodes, 'and');
+    return emit(node, { optimizeQuotes: true });
+  },
+};
+
+/**
  * RSQL 数据过滤器
  *
  * https://github.com/piotr-oles/rsql
@@ -21,49 +72,6 @@ export const useRSQLFilter = () => {
     const node = rsqlBuilder.logic(rsqlFilters, 'and');
     return emit(node, { optimizeQuotes: true });
   }, [rsqlFilters]);
-
-  const rsqlOps = {
-    ...rsqlBuilder,
-    contains(selector: string, value: string) {
-      return rsqlBuilder.comparison(selector, '=contains=', value);
-    },
-    icontains(selector: string, value: string) {
-      return rsqlBuilder.comparison(selector, '=icontains=', value);
-    },
-    between(selector: string, values: (string | number)[]) {
-      if (values.length != 2) {
-        throw new Error('between 操作符数据必须有两个值');
-      }
-      return rsqlBuilder.comparison(selector, '=between=', values);
-    },
-    comparisonEx(
-      selector: string,
-      operator:
-        | '=='
-        | '!='
-        | '<='
-        | '>='
-        | '<'
-        | '>'
-        | '=in='
-        | '=out='
-        | '=le='
-        | '=ge='
-        | '=lt='
-        | '=gt='
-        | '=contains='
-        | '=icontains=',
-      value: string | number | (string | number)[] | undefined | null
-    ): ComparisonNode | null {
-      if (typeof value === 'undefined' || value === null || value === '') {
-        return null;
-      }
-      if (Array.isArray(value) && value.length <= 0) {
-        return null;
-      }
-      return rsqlBuilder.comparison(selector, operator, value);
-    },
-  };
 
   const setRSQLFilters = (nodes: Array<ExpressionNode | null>) => {
     const arr: ExpressionNode[] = [];
