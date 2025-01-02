99d5f28ec469c8e15ad6aa0b9d38dcdf6be9a816
ZY <kzou227@qq.com>
2025-01-02 18:28:19 +0800

    前端新增 RSQL

--- mahu-ui/mahu-admin-ui/src/hooks/useDataFilter.ts
+++ mahu-ui/mahu-admin-ui/src/hooks/useDataFilter.ts
@@ -1,10 +1,88 @@
 import { useMemo, useState } from 'react';
 import dayjs from 'dayjs';
+import rsqlBuilder from '@rsql/builder';
+import { ExpressionNode, getSelector, isComparisonNode, isLogicNode } from '@rsql/ast';
+import { emit } from '@rsql/emitter';
 
 type Filter = {
   value: unknown;
   qname: string;
   op: 'eq' | 'ne' | 'gt' | 'lt' | 'gte' | 'lte' | 'contains' | 'icontains' | 'in' | 'nin' | 'between';
+};
+
+/**
+ * RSQL 数据过滤器
+ *
+ * https://github.com/piotr-oles/rsql
+ *
+ * @returns
+ */
+export const useRSQLFilter = () => {
+  const [rsqlFilters, setRSQLFilters] = useState<ExpressionNode[]>([]);
+
+  const queryFilter = useMemo(() => {
+    const node = rsqlBuilder.logic(rsqlFilters, 'and');
+    return emit(node, { optimizeQuotes: true });
+  }, [rsqlFilters]);
+
+  const rsqlOps = {
+    ...rsqlBuilder,
+    like(selector: string, value: string) {
+      return rsqlBuilder.comparison(selector, '=like=', value);
+    },
+    ilike(selector: string, value: string) {
+      return rsqlBuilder.comparison(selector, '=ilike=', value);
+    },
+    between(selector: string, values: (string | number)[]) {
+      if (values.length != 2) {
+        throw new Error('between 操作符数据必须有两个值');
+      }
+      return rsqlBuilder.comparison(selector, '=between=', values);
+    },
+  };
+
+  /// 更新删除数据过滤器
+  const upsertRSQLFilters = (filters: ExpressionNode[]) => {
+    const arr = [...filters];
+    for (const f of rsqlFilters) {
+      if (isLogicNode(f)) {
+        continue;
+      }
+
+      const idx = arr.findIndex((o) => {
+        if (isComparisonNode(o)) {
+          const selector = getSelector(o);
+          return selector === getSelector(f);
+        }
+        return false;
+      });
+
+      if (idx === -1) {
+        arr.push(f);
+      }
+    }
+    return setRSQLFilters(filters);
+  };
+
+  /// 删除数据过滤
+  const removeRSQLFilters = (selectors: string[]) => {
+    const filters = rsqlFilters.filter((o) => {
+      if (isLogicNode(o)) {
+        return false;
+      }
+      const selector = getSelector(o);
+      return selectors.includes(selector);
+    });
+    setRSQLFilters(filters);
+  };
+
+  return {
+    queryFilter,
+    rsqlOps,
+    setRSQLFilters,
+    upsertRSQLFilters,
+    removeRSQLFilters,
+  };
 };
 
 /**
