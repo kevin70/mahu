35e1f26dd23eaddfff8d27a6d8786baa98662775
kk <kzou227@qq.com>
2025-01-24 23:38:37 +0800

    增加加载状态

--- mahu-ui/mahu-admin-ui/src/components/mart/HMartAttributeMeta.tsx
+++ mahu-ui/mahu-admin-ui/src/components/mart/HMartAttributeMeta.tsx
@@ -1,6 +1,7 @@
 import { MART_API } from '@/services';
 import { GetMartAttributeResponse } from '@/services/generated';
 import { useQuery } from '@tanstack/react-query';
+import { Spin } from 'antd';
 import { ReactNode } from 'react';
 
 interface HMartAttributeMetaProps {
@@ -10,7 +11,7 @@ interface HMartAttributeMetaProps {
 
 export const HMartAttributeMeta = ({ attributeId, children }: HMartAttributeMetaProps) => {
   const { data } = useQuery({
-    queryKey: ['HMartAttributeMeta', attributeId],
+    queryKey: [`HMartAttributeMeta`, attributeId],
     async queryFn() {
       return MART_API.getMartAttribute({
         id: attributeId,
@@ -21,5 +22,5 @@ export const HMartAttributeMeta = ({ attributeId, children }: HMartAttributeMeta
   if (data) {
     return children(data);
   }
-  return <>属性加载错误</>;
+  return <Spin />;
 };
