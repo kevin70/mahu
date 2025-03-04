3e3775b3177d1d4a80363ca7db1ca5e3f3397165
ZY <kzou227@qq.com>
2025-03-04 17:43:21 +0800

    U

---
+++ mahu-admin/mahu-admin-web/src/main/java/cool/houge/mahu/admin/controller/mart/PMartController.java
@@ -0,0 +1,22 @@
+package cool.houge.mahu.admin.controller.mart;
+
+import cool.houge.mahu.admin.internal.VoBeanMapper;
+import io.helidon.webserver.http.HttpRules;
+import io.helidon.webserver.http.HttpService;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+/// 公共数据接口
+///
+/// @author ZY (kzou227@qq.com)
+@Singleton
+public class PMartController implements HttpService {
+
+    @Inject
+    VoBeanMapper beanMapper;
+
+    @Override
+    public void routing(HttpRules rules) {
+        //
+    }
+}
