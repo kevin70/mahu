b716b238f9101c9a719fa712d614f431c6216ec3
kk <kzou227@qq.com>
2025-03-30 14:42:49 +0800

    增加文档注释

--- mahu-common/src/main/java/cool/houge/mahu/common/BizCode.java
+++ mahu-common/src/main/java/cool/houge/mahu/common/BizCode.java
@@ -2,7 +2,9 @@ package cool.houge.mahu.common;
 
 import java.util.Optional;
 
-/// 业务错误码
+/// 业务错误码。
+///
+/// 系统错误码采用32位纯数字格式，其中常规错误码为6位数。请注意，以数字“1”开头的6位错误码为系统保留码段，自定义错误码时应避免使用此类编号。
 ///
 /// @author ZY (kzou227@qq.com)
 public interface BizCode {
--- mahu-common/src/main/java/cool/houge/mahu/common/BizCodes.java
+++ mahu-common/src/main/java/cool/houge/mahu/common/BizCodes.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
-/// 业务错误状态定义及使用
+/// 业务错误状态定义及使用。
+///
+/// 系统错误码采用32位纯数字格式，其中常规错误码为6位数。请注意，以数字“1”开头的6位错误码为系统保留码段，自定义错误码时应避免使用此类编号。
 ///
 /// [参考 gRPC 错误状态码实现](https://github.com/grpc/grpc/blob/master/doc/statuscodes.md)
 /// @author ZY (kzou227@qq.com)
