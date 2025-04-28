97b6a7cd05937f7c6e7a3f6ee4e0ffb4e1f0433f
ZY <kzou227@qq.com>
2025-04-28 11:52:11 +0800

    完善单元测试

--- mahu-admin/mahu-admin-infra/src/test/java/cool/houge/mahu/admin/system/repository/AuditPersistControllerTest.java
+++ mahu-admin/mahu-admin-infra/src/test/java/cool/houge/mahu/admin/system/repository/AuditPersistControllerTest.java
@@ -11,15 +11,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 ///
 /// @author ZY (kzou227@qq.com)
 class AuditPersistControllerTest extends TestTransactionBase {
 
     @Test
     void changeData() {
+        var employeeId = 1;
+
         var server = (DefaultServer) db();
         var descriptor = server.descriptor(Employee.class);
-        var dbEntity = server.find(Employee.class, 1);
+        var dbEntity = server.find(Employee.class, employeeId);
         dbEntity.setNickname("abc")
                 .setAvatar("TEST")
                 .setDepartment(new Department().setId(1).setName("WORLD"))
@@ -30,5 +34,10 @@ class AuditPersistControllerTest extends TestTransactionBase {
         var auditPersistController = new AuditPersistController();
         var auditLog = new AdminAuditLog();
         auditPersistController.changedData(server, descriptor, (EntityBean) dbEntity, auditLog);
+
+        assertThat(auditLog.getData())
+                .isEqualTo(
+                        "{\"nickname\":\"abc\",\"avatar\":\"TEST\",\"department\":{\"id\":1},\"roles\":[{\"id\":1,\"name\":\"HELLO\"},{\"id\":2,\"name\":\"WORLD\"}]}");
+        assertThat(auditLog.getOldData()).isNotEmpty();
     }
 }
