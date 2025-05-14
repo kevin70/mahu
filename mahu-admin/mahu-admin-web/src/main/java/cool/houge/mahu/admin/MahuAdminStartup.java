d089adb60d4c171ef0ff9bfb3959087596ac07a6
ZY <kzou227@qq.com>
2025-05-14 13:48:04 +0800

    去除 PID

--- mahu-admin/mahu-admin-web/src/main/java/cool/houge/mahu/admin/MahuAdminStartup.java
+++ mahu-admin/mahu-admin-web/src/main/java/cool/houge/mahu/admin/MahuAdminStartup.java
@@ -4,8 +4,6 @@ import io.avaje.inject.BeanScope;
 import io.helidon.Main;
 import io.helidon.config.Config;
 import io.helidon.spi.HelidonStartupProvider;
-import java.util.Set;
-import java.util.StringJoiner;
 
 /// 启动后端应用程序
 ///
@@ -18,50 +16,12 @@ public class MahuAdminStartup implements HelidonStartupProvider {
     @Override
     public void start(String[] args) {
         System.setProperty("ebean.registerShutdownHook", "false");
-        if (args.length >= 1 && "stop".equals(args[0])) {
-            stop();
-            System.exit(0);
-            return;
-        }
 
-        beanScope = BeanScope.builder()
+        this.beanScope = BeanScope.builder()
                 .bean(Config.class, Config.create())
                 .shutdownHook(false)
                 .build();
         Main.addShutdownHandler(beanScope::close);
-    }
-
-    private static void stop() {
-        var current = ProcessHandle.current();
-        var currentInfo = current.info();
-        ProcessHandle.allProcesses()
-                .filter(p -> p.pid() != current.pid())
-                .filter(process -> {
-                    var info = process.info();
-                    if (!info.user().equals(currentInfo.user())) {
-                        return false;
-                    }
-                    if (!info.command().equals(currentInfo.command())) {
-                        return false;
-                    }
-
-                    var args = info.arguments().map(Set::of).orElse(Set.of());
-                    var currentArgs = currentInfo.arguments().map(Set::of).orElse(Set.of());
-                    return currentArgs.containsAll(args);
-                })
-                .forEach(process -> {
-                    var msg = new StringJoiner(" ");
-                    msg.add("停止进程 PID:");
-                    msg.add(String.valueOf(process.pid()));
-                    process.info().arguments().ifPresent(args -> {
-                        for (String arg : args) {
-                            msg.add(arg);
-                        }
-                    });
-
-                    System.out.println(msg);
-                    process.destroy();
-                });
     }
 
     public static void main(String[] args) {
