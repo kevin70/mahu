8c7baff63a53a08cbabec6b367772212eeb229d6
ZY <kzou227@qq.com>
2025-05-20 15:21:34 +0800

    优化代码

--- mahu-admin/mahu-admin-infra/src/main/java/cool/houge/mahu/admin/DbBeanFactory.java
+++ mahu-admin/mahu-admin-infra/src/main/java/cool/houge/mahu/admin/DbBeanFactory.java
@@ -47,9 +47,10 @@ public class DbBeanFactory {
     public AppInstance appInstance(InfoConfig config, DataSource dataSource) {
         var nodeIdProvider = new DatabaseNodeIdProvider("admin_node_seq", dataSource);
         // 此处实现代表集群应用实例最多 256 个节点，如果节点超出 256，应该修改此处的逻辑
-        var nodeId = nodeIdProvider.getNodeId() % 256 + 1;
+        var maxNode = 256;
+        var nodeId = nodeIdProvider.getNodeId() % maxNode + 1;
         System.setProperty("tsid.node", String.valueOf(nodeId));
-        System.setProperty("tsid.node.count", "256");
+        System.setProperty("tsid.node.count", String.valueOf(maxNode));
         return new AppInstance(config.name(), config.version(), nodeId);
     }
 
