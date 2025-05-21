6e388b0340a7f63436cda246045b674d903ac8cf
ZY <kzou227@qq.com>
2025-05-21 17:51:34 +0800

    优化MQ消费逻辑

--- mahu-admin/mahu-admin-infra/src/test/java/cool/houge/mahu/admin/mq/MQConsumerTest.java
+++ mahu-admin/mahu-admin-infra/src/test/java/cool/houge/mahu/admin/mq/MQConsumerTest.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.avaje.inject.test.InjectTest;
 import jakarta.inject.Inject;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.jupiter.api.Test;
 
@@ -23,14 +22,15 @@ class MQConsumerTest {
     void addConsumer() {
         var queue = "topic.queue.roar.mahu.send_gift_v2";
         var exchange = "gift";
-        var routerKey = "send_gift_v2.test";
+        var routerKey = "send_gift_v2.TEST";
         var bodyType = TypeFactory.defaultInstance().constructType(new TypeReference<Map<String, Object>>() {});
 
         var state = new AtomicBoolean(false);
         mqConsumer.addConsumer(queue, exchange, routerKey, bodyType, (message) -> {
-            System.out.println(message);
+            var thread = Thread.currentThread();
+            System.out.println(thread.getName() + ": " + message);
+            state.set(true);
         });
-
-        await().atMost(30, TimeUnit.MINUTES).untilTrue(state);
+        await().untilTrue(state);
     }
 }
