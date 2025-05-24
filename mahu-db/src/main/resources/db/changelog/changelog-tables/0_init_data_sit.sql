636df93f9890893790267d7650c1549ed9f6e66e
kk <kzou227@qq.com>
2025-05-24 16:55:33 +0800

    新增SIT环境测试部门数据

---
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/0_init_data_sit.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+
+
+-- changeset kzou227@qq.com:SIT.202505232109 context:sit
+-- comment: SIT专用测试数据
+INSERT INTO system.department (id, created_at, updated_at, name, parent_id, leader_id, ordering)
+VALUES (200, current_time, current_time, '测试部', null, 1, 0);
+INSERT INTO system.department (id, created_at, updated_at, name, parent_id, leader_id, ordering)
+VALUES (201, current_time, current_time, '测试1部', 200, null, 1);
+INSERT INTO system.department (id, created_at, updated_at, name, parent_id, leader_id, ordering)
+VALUES (202, current_time, current_time, '测试2部', 200, null, 2);
+--
