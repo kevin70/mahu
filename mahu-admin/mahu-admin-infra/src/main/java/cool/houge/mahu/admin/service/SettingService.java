3cc6821c61ed636bd865605da8a72945ac6bd79c
ZY <kzou227@qq.com>
2025-05-28 15:52:04 +0800

    删除多余代码

--- mahu-admin/mahu-admin-infra/src/main/java/cool/houge/mahu/admin/service/SettingService.java
+++ mahu-admin/mahu-admin-infra/src/main/java/cool/houge/mahu/admin/service/SettingService.java
@@ -1,15 +1,9 @@
 package cool.houge.mahu.admin.service;
 
-import com.google.common.io.Files;
-import cool.houge.mahu.config.OssConfig;
-import cool.houge.mahu.remote.aliyun.AliyunClient;
-import cool.houge.mahu.remote.aliyun.DirectUploadRequest;
 import io.helidon.config.Config;
-import io.hypersistence.tsid.TSID;
+import io.minio.MinioClient;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import java.util.List;
-import java.util.Optional;
 
 ///
 /// @author ZY (kzou227@qq.com)
@@ -17,41 +11,16 @@ import java.util.Optional;
 public class SettingService {
 
     private final Config config;
-    private final AliyunClient aliyunClient;
+    private final MinioClient minioClient;
 
     @Inject
-    public SettingService(Config config, AliyunClient aliyunClient) {
+    public SettingService(Config config, MinioClient minioClient) {
         this.config = config;
-        this.aliyunClient = aliyunClient;
+        this.minioClient = minioClient;
     }
 
     /// 构建 OSS 直接上传参数
     public MakeOssDirectUploadResult makeOssDirectUpload(MakeOssDirectUploadPayload payload) {
-        var ossConfig = OssConfig.create(
-                config.get(OssConfig.PREFIX + "." + payload.getKind().name()));
-        var ext = Files.getFileExtension(payload.getFileName());
-        var key = aliyunClient.joinFileKey(
-                List.of(
-                        ossConfig.keyPrefix(),
-                        Optional.ofNullable(payload.getPrefixLimit()).orElse(""),
-                        TSID.fast().toString()),
-                ext);
-        var request = new DirectUploadRequest()
-                .setBucket(ossConfig.bucket())
-                .setKey(key)
-                .setAccessKeyId(ossConfig.accessKeyId())
-                .setAccessKeySecret(ossConfig.accessKeySecret())
-                .setMinFileSize(ossConfig.minFileSize())
-                .setMaxFileSize(ossConfig.maxFileSize())
-                .setDurationSeconds(ossConfig.durationSeconds());
-        var response = aliyunClient.directUploadSignature(request);
-
-        return new MakeOssDirectUploadResult()
-                .setAccessUrl(ossConfig.endpoint() + "/" + response.getKey())
-                .setEndpoint(ossConfig.endpoint())
-                .setKey(key)
-                .setPolicy(response.getPolicy())
-                .setAccessKeyId(ossConfig.accessKeyId())
-                .setSignature(response.getSignature());
+        return null;
     }
 }
