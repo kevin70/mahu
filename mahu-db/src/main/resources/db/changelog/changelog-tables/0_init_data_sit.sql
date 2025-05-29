744f25b3f5ee538db242dfba082824d8501daa98
ZY <kzou227@qq.com>
2025-05-29 15:49:47 +0800

    SIT 测试数据

--- mahu-db/src/main/resources/db/changelog/changelog-tables/0_init_data_sit.sql
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/0_init_data_sit.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
 
--- changeset kzou227@qq.com:SIT.202505232109 context:sit
+-- changeset kzou227@qq.com:SIT.202505232109 context:@sit
 -- comment: SIT专用测试数据
 INSERT INTO system.department (id, created_at, updated_at, name, parent_id, leader_id, ordering)
 VALUES (200, current_timestamp(0), current_timestamp(0), '测试部', null, 1, 0);
