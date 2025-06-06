f8275d9cea98c65acdc8689b3cafcd8eb2bfd6df
ZY <kzou227@qq.com>
2025-06-06 17:49:03 +0800

    新增 openapi-ts

--- mahu-ui/mahu-admin-ui/openapi-ts.config.ts
+++ mahu-ui/mahu-admin-ui/openapi-ts.config.ts
@@ -2,17 +2,22 @@ import { defineConfig, defaultPlugins } from '@hey-api/openapi-ts';
 
 export default defineConfig({
   input: {
-    // filters: {
-    //   operations: {
-    //     exclude: ['GET /samples'],
-    //   },
-    // },
     path: `dist/openapi.yaml`,
   },
   output: {
     format: 'prettier',
     lint: 'eslint',
     path: './src/client',
+    //case: 'camelCase',
   },
-  plugins: [...defaultPlugins, '@hey-api/client-axios', '@tanstack/react-query'],
+
+  plugins: [
+    ...defaultPlugins,
+    '@hey-api/client-axios',
+    '@tanstack/react-query',
+    {
+      dates: true,
+      name: '@hey-api/transformers',
+    },
+  ],
 });
