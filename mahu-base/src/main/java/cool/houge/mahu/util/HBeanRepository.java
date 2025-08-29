6aa9cb0faf288ea9d6b569339d5f53369ad7d0ae
ZY <kzou227@qq.com>
2025-08-29 14:19:25 +0800

    完善对象搜索

--- mahu-base/src/main/java/cool/houge/mahu/util/HBeanRepository.java
+++ mahu-base/src/main/java/cool/houge/mahu/util/HBeanRepository.java
@@ -13,10 +13,11 @@ import cz.jirutka.rsql.parser.RSQLParserException;
 import io.ebean.BeanRepository;
 import io.ebean.Database;
 import io.ebean.typequery.QueryBean;
-import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jspecify.annotations.NonNull;
+
+import java.util.List;
 
 /// 扩展数据管理基类
 ///
@@ -35,7 +36,7 @@ public class HBeanRepository<I, T> extends BeanRepository<I, T> {
     /// 获取指定ID的数据，如果数据不存在则抛出[BizCodes#NOT_FOUND]
     ///
     /// @param id 数据ID
-    public @NonNull T obtainById(@NonNull I id) throws BizCodeException {
+    public @NonNull T loadById(@NonNull I id) throws BizCodeException {
         var bean = findById(id);
         if (bean == null) {
             throw new BizCodeException(BizCodes.NOT_FOUND, "未找到指定的数据[" + id + "] " + type);
@@ -43,12 +44,36 @@ public class HBeanRepository<I, T> extends BeanRepository<I, T> {
         return bean;
     }
 
+    /// 可搜索项
+    protected @NonNull List<FilterItem> filterableItems() {
+        return List.of();
+    }
+
+    /// 可排序项
+    protected @NonNull List<FilterItem> sortableItems() {
+        return filterableItems();
+    }
+
     /// 将过滤条件应用到查询上
     ///
     /// @param query        查询对象
     /// @param filter       数据过滤条件
-    /// @param filterItems 过滤字段
-    protected void apply(QueryBean<?, ?> query, DataFilter filter, List<FilterItem> filterItems) {
+    /// @param page 分页参数
+    protected final void apply(QueryBean<@NonNull T, ?> query, DataFilter filter, Pageable page) {
+        this.apply(query, filter);
+        this.apply(query, page);
+    }
+
+    /// 将过滤条件应用到查询上
+    ///
+    /// @param query        查询对象
+    /// @param filter       数据过滤条件
+    protected final void apply(QueryBean<@NonNull T, ?> query, DataFilter filter) {
+        var filterItems = filterableItems();
+        if (filterItems.isEmpty()) {
+            throw new IllegalStateException("可过滤项为空");
+        }
+
         var ctx = new RSQLContext(filterItems, query);
         if (filter.filter() != null && !filter.filter().isEmpty()) {
             try {
@@ -58,9 +83,6 @@ public class HBeanRepository<I, T> extends BeanRepository<I, T> {
                 throw new BizCodeException(BizCodes.INVALID_ARGUMENT, "RSQL解析错误", e);
             }
         }
-
-        this.applyPaging(query, filter);
-        this.applySort(query, filter, ctx);
 
         // 包含软删除的数据
         if (filter.isIncludeDeleted()) {
@@ -72,20 +94,21 @@ public class HBeanRepository<I, T> extends BeanRepository<I, T> {
     ///
     /// @param query 查询对象
     /// @param page 分页参数
-    protected void apply(QueryBean<?, ?> query, Pageable page) {
-        this.apply(query, page, List.of());
-    }
-
-    protected void apply(QueryBean<?, ?> query, Pageable page, List<FilterItem> sortItems) {
+    protected final void apply(QueryBean<@NonNull T, ?> query, Pageable page) {
         if (page.isPaged()) {
             query.setFirstRow((int) page.getOffset()).setMaxRows(page.getPageSize());
         }
-        this.apply(query, page.getSort(), sortItems);
+        this.apply(query, page.getSort());
     }
 
-    protected void apply(QueryBean<?, ?> query, Sort sort, List<FilterItem> items) {
+    protected final void apply(QueryBean<@NonNull T, ?> query, Sort sort) {
         if (sort.isUnsorted()) {
             return;
+        }
+
+        var items = sortableItems();
+        if (items.isEmpty()) {
+            throw new IllegalStateException("可排序项为空");
         }
         for (FilterItem item : items) {
             var o = sort.getOrderFor(item.getKey());
@@ -96,40 +119,6 @@ public class HBeanRepository<I, T> extends BeanRepository<I, T> {
                 query.query().orderBy().asc(item.getColumn());
             } else {
                 query.query().orderBy().desc(item.getColumn());
-            }
-        }
-    }
-
-    /// 将分页参数应用到查询上
-    ///
-    /// @param query 查询对象
-    /// @param paging 分页参数
-    protected void applyPaging(QueryBean<?, ?> query, Paging paging) {
-        if (paging.offset() > 0) {
-            query.setFirstRow(paging.offset());
-        }
-
-        if (paging.limit() > 0) {
-            query.setMaxRows(paging.limit());
-        }
-    }
-
-    void applySort(QueryBean<?, ?> query, DataFilter filter, RSQLContext ctx) {
-        for (String s : filter.sorts()) {
-            if (s == null || s.isEmpty()) {
-                continue;
-            }
-
-            boolean ascending = s.charAt(0) != '-';
-            String name = ascending ? s : s.substring(1);
-            var field = ctx.getFilterField(name);
-            if (field != null) {
-                log.debug("找到排序属性 {}: {}", name, field);
-                if (ascending) {
-                    query.query().orderBy().asc(field.getColumn());
-                } else {
-                    query.query().orderBy().desc(field.getColumn());
-                }
             }
         }
     }
