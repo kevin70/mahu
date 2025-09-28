e4475994cf6a443e56ecbc237049a75a47e7efea
ZY <kzou227@qq.com>
2025-09-28 18:46:28 +0800

    重构

--- mahu-base/src/main/java/cool/houge/mahu/util/HBeanRepository.java
+++ mahu-base/src/main/java/cool/houge/mahu/util/HBeanRepository.java
@@ -1,7 +1,5 @@
 package cool.houge.mahu.util;
 
-import static java.util.Objects.requireNonNull;
-
 import cool.houge.mahu.BizCodeException;
 import cool.houge.mahu.BizCodes;
 import cool.houge.mahu.domain.DataFilter;
@@ -59,26 +57,49 @@ public class HBeanRepository<I, T> extends BeanRepository<@NonNull I, @NonNull T
     /// @param sortItems 可排序项
     protected final void apply(
             Query<T> query, DataFilter dataFilter, List<FilterItem> filterItems, List<FilterItem> sortItems) {
-        requireNonNull(filterItems);
         // 包含软删除的数据
         if (dataFilter.includeDeleted()) {
             query.setIncludeSoftDeletes();
         }
 
-        if (!filterItems.isEmpty()) {
-            dataFilter.query().filter(s -> !s.isEmpty()).ifPresent(q -> {
-                try {
-                    var ctx = new RSQLContext(filterItems, query);
-                    var node = RSQL_PARSER.parse(q);
-                    node.accept(new EBeanRSQLVisitor(), ctx);
-                } catch (RSQLParserException e) {
-                    throw new BizCodeException(BizCodes.INVALID_ARGUMENT, "RSQL 解析错误", e);
-                }
-            });
+        if (dataFilter.query().isPresent()) {
+            this.applyFilter(query, dataFilter.query().get(), filterItems);
         }
 
-        // 应用分页
-        this.apply(query, dataFilter.page(), sortItems);
+        if (sortItems.isEmpty()) {
+            if (!dataFilter.page().isUnpaged()) {
+                this.applyPage(query, dataFilter.page());
+            }
+        } else {
+            if (dataFilter.page().isUnpaged()) {
+                this.applySort(query, dataFilter.page().getSort(), sortItems);
+            } else {
+                this.applyPage(query, dataFilter.page(), sortItems);
+            }
+        }
+    }
+
+    /// 将分页参数应用到查询上
+    ///
+    /// @param query 查询对象
+    /// @param filter RSQL 过滤查询
+    /// @param filterItems 可过滤项
+    /// @return this
+    protected final HBeanRepository<I, T> applyFilter(Query<T> query, String filter, List<FilterItem> filterItems) {
+        if (filterItems == null || filterItems.isEmpty()) {
+            throw new IllegalArgumentException("filterItems 不能为空");
+        }
+
+        if (filter != null && !filter.isEmpty()) {
+            try {
+                var ctx = new RSQLContext(filterItems, query);
+                var node = RSQL_PARSER.parse(filter);
+                node.accept(new EBeanRSQLVisitor(), ctx);
+            } catch (RSQLParserException e) {
+                throw new BizCodeException(BizCodes.INVALID_ARGUMENT, "RSQL 解析错误", e);
+            }
+        }
+        return this;
     }
 
     /// 将分页参数应用到查询上
@@ -86,35 +107,37 @@ public class HBeanRepository<I, T> extends BeanRepository<@NonNull I, @NonNull T
     /// @param query 查询对象
     /// @param page 分页参数
     /// @param sortItems 可排序项
-    protected final void apply(Query<T> query, Pageable page, List<FilterItem> sortItems) {
-        this.apply(query, page);
-        this.apply(query, page.getSort(), sortItems);
+    /// @return this
+    protected final HBeanRepository<I, T> applyPage(Query<T> query, Pageable page, List<FilterItem> sortItems) {
+        return applyPage(query, page).applySort(query, page.getSort(), sortItems);
     }
 
     /// 将分页参数应用到查询上
     ///
     /// @param query 查询对象
     /// @param page 分页参数
-    protected final void apply(Query<T> query, Pageable page) {
-        if (!page.isUnpaged()) {
-            query.setFirstRow((int) page.getOffset()).setMaxRows(page.getPageSize());
-        }
+    /// @return this
+    protected final HBeanRepository<I, T> applyPage(Query<T> query, Pageable page) {
+        query.setFirstRow((int) page.getOffset()).setMaxRows(page.getPageSize());
+        return this;
     }
 
     /// 将排序参数应用到查询上
     ///
-    /// @param query 查询对象
-    /// @param sort 排序参数
+    /// @param query     查询对象
+    /// @param sort      排序参数
     /// @param sortItems 可排序项
-    protected final void apply(Query<T> query, Sort sort, List<FilterItem> sortItems) {
-        requireNonNull(sortItems);
+    /// @return this
+    protected final HBeanRepository<I, T> applySort(Query<T> query, Sort sort, List<FilterItem> sortItems) {
+        if (sortItems == null || sortItems.isEmpty()) {
+            throw new IllegalArgumentException("sortItems 不能为空");
+        }
         if (sort.isSorted()) {
             for (FilterItem item : sortItems) {
                 var o = sort.getOrderFor(item.getKey());
                 if (o == null) {
                     continue;
                 }
-
                 if (o.getDirection() == Sort.Direction.ASC) {
                     query.orderBy().asc(item.getColumn());
                 } else {
@@ -122,5 +145,6 @@ public class HBeanRepository<I, T> extends BeanRepository<@NonNull I, @NonNull T
                 }
             }
         }
+        return this;
     }
 }
