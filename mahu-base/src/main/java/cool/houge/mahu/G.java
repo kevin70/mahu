56411fdf212f693718fa1cc9035a0fe1179925cb
ZY <kzou227@qq.com>
2025-11-21 14:22:54 +0800

    增加全局变量配置

---
+++ mahu-base/src/main/java/cool/houge/mahu/G.java
@@ -0,0 +1,11 @@
+package cool.houge.mahu;
+
+import lombok.experimental.UtilityClass;
+
+/// 公共的全局变量
+@UtilityClass
+public final class G {
+
+    /// MDC 日志追踪ID名称
+    public static final String MDC_TRACE_ID = "TRACE_ID";
+}
