9d8a5616ae26ee2d724a69491a13e7aa3a8a65b1
kk <kzou227@qq.com>
2025-11-23 22:36:55 +0800

    新增功能、延迟消息数据库表

---
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.delay_message.sql
@@ -0,0 +1,52 @@
+--
+-- liquibase formatted sql
+-- changeset kzou227@qq.com:202511232235
+CREATE TABLE sys.delay_message (
+  id UUID NOT NULL PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  topic VARCHAR(50),
+  status SMALLINT,
+  delay_until TIMESTAMP,
+  delay INTEGER,
+  multiplier SMALLINT,
+  max_delay INTEGER,
+  max_attempts SMALLINT,
+  retry_count SMALLINT,
+  body JSONB,
+  feature_id INTEGER
+);
+
+comment ON TABLE sys.delay_message IS '延迟消息';
+
+comment ON COLUMN sys.delay_message.id IS '主键';
+
+comment ON COLUMN sys.delay_message.created_at IS '创建时间';
+
+comment ON COLUMN sys.delay_message.updated_at IS '更新时间';
+
+comment ON COLUMN sys.delay_message.topic IS '主题';
+
+comment ON COLUMN sys.delay_message.status IS '状态';
+
+comment ON COLUMN sys.delay_message.delay_until IS '消息延迟到的绝对时间（精确到毫秒）';
+
+comment ON COLUMN sys.delay_message.delay IS '初始重试间隔（1000 毫秒，即 1 秒）';
+
+comment ON COLUMN sys.delay_message.multiplier IS '重试间隔的倍数（指数退避策略，间隔依次为 1s、2s、4s、8s...）';
+
+comment ON COLUMN sys.delay_message.max_delay IS '最大重试间隔（10000 毫秒，即 10 秒，避免间隔无限增大）';
+
+comment ON COLUMN sys.delay_message.max_attempts IS '最大重试次数（包括首次执行）';
+
+comment ON COLUMN sys.delay_message.retry_count IS '重试次数';
+
+comment ON COLUMN sys.delay_message.body IS '消息内容';
+
+comment ON COLUMN sys.delay_message.feature_id IS '功能 ID（用于追踪统计）';
+
+CREATE INDEX delay_message_status_delay_until_i ON sys.delay_message (status, delay_until);
+
+CREATE INDEX delay_message_topic_i ON sys.delay_message (topic);
+
+-- rollback drop table sys.delay_message;
---
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.feature.sql
@@ -0,0 +1,58 @@
+--
+-- liquibase formatted sql
+-- changeset kzou227@qq.com:202511232228
+CREATE TABLE sys.feature (
+  id INTEGER NOT NULL CONSTRAINT feature_pk PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  module VARCHAR(50),
+  code VARCHAR(50),
+  name VARCHAR(100),
+  description VARCHAR(1024),
+  status SMALLINT,
+  effective_from TIMESTAMP,
+  effective_to TIMESTAMP,
+  start_time TIME,
+  end_time TIME,
+  weekdays JSONB,
+  allow_user_rb bytea,
+  deny_user_rb bytea,
+  extra_properties JSONB,
+  extra_schema JSONB
+);
+
+comment ON COLUMN sys.feature.id IS '主键';
+
+comment ON COLUMN sys.feature.created_at IS '创建时间';
+
+comment ON COLUMN sys.feature.updated_at IS '更新时间';
+
+comment ON COLUMN sys.feature.module IS '模块';
+
+comment ON COLUMN sys.feature.code IS '功能代码';
+
+comment ON COLUMN sys.feature.name IS '名称';
+
+comment ON COLUMN sys.feature.description IS '描述';
+
+comment ON COLUMN sys.feature.status IS '状态';
+
+comment ON COLUMN sys.feature.effective_from IS '生效开始时间（精确到秒）';
+
+comment ON COLUMN sys.feature.effective_to IS '生效结束时间（精确到秒）';
+
+comment ON COLUMN sys.feature.start_time IS '每天的开始时间（精确到秒）';
+
+comment ON COLUMN sys.feature.end_time IS '每天的结束时间（精确到秒，结束时间小于开始时间代表跨天）';
+
+comment ON COLUMN sys.feature.weekdays IS '启用的星期';
+
+comment ON COLUMN sys.feature.allow_user_rb IS '可用的用户';
+
+comment ON COLUMN sys.feature.deny_user_rb IS '禁用的用户';
+
+comment ON COLUMN sys.feature.extra_properties IS '扩展属性';
+
+comment ON COLUMN sys.feature.extra_schema IS 'JSON Schema';
+
+-- rollback drop table sys.feature;
