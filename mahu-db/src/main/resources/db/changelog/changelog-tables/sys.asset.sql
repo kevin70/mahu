7b28b412c2341965de52fd76ec187b6f008b1ac7
ZY <kzou227@qq.com>
2025-11-24 15:47:35 +0800

    新增资源证件照数据库表设计

---
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.asset.sql
@@ -0,0 +1,33 @@
+--
+-- liquibase formatted sql
+-- changeset kzou227@qq.com:202511241531
+CREATE TABLE sys.asset (
+  id bigserial CONSTRAINT asset_pk PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  uid BIGINT NOT NULL,
+  type SMALLINT DEFAULT 0 NOT NULL,
+  status SMALLINT NOT NULL,
+  object_name VARCHAR(255) NOT NULL,
+  metadata JSONB
+);
+
+comment ON TABLE sys.asset IS '资源表';
+
+comment ON COLUMN sys.asset.id IS '主键';
+
+comment ON COLUMN sys.asset.created_at IS '创建时间';
+
+comment ON COLUMN sys.asset.updated_at IS '更新时间';
+
+comment ON COLUMN sys.asset.uid IS '用户 ID';
+
+comment ON COLUMN sys.asset.type IS '资源类型';
+
+comment ON COLUMN sys.asset.status IS '状态';
+
+comment ON COLUMN sys.asset.object_name IS 'OSS 对象名称';
+
+comment ON COLUMN sys.asset.metadata IS '可以用来存储额外的元数据，比如图片尺寸、背景颜色、格式等。 ';
+
+-- rollback drop table sys.asset;
---
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.id_photo.sql
@@ -0,0 +1,39 @@
+--
+-- liquibase formatted sql
+-- changeset kzou227@qq.com:202511241532
+CREATE TABLE sys.id_photo (
+  id bigserial CONSTRAINT id_photo_pk PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  uid BIGINT NOT NULL,
+  type SMALLINT DEFAULT 0 NOT NULL,
+  status SMALLINT NOT NULL,
+  object_name VARCHAR(255) NOT NULL,
+  metadata JSONB
+);
+
+comment ON COLUMN sys.id_photo.id IS '主键';
+
+comment ON COLUMN sys.id_photo.created_at IS '创建时间';
+
+comment ON COLUMN sys.id_photo.updated_at IS '更新时间';
+
+comment ON COLUMN sys.id_photo.uid IS '用户 ID';
+
+comment ON COLUMN sys.id_photo.type IS '证件类型
+- 0：证件照片
+- 1：身份证头像面
+- 2：身份证国徽面
+- 3：护照';
+
+comment ON COLUMN sys.id_photo.status IS '状态';
+
+comment ON COLUMN sys.id_photo.object_name IS 'OSS 对象名称';
+
+comment ON COLUMN sys.id_photo.metadata IS '可以用来存储额外的元数据，比如图片尺寸、背景颜色、格式等。 ';
+
+CREATE INDEX id_photo_uid_i ON sys.id_photo (uid);
+
+CREATE INDEX id_photo_status_created_at_i ON sys.id_photo (status, created_at);
+
+-- rollback drop table sys.id_photo;
