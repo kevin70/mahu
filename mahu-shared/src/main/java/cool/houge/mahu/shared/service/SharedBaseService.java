82d4e838859157e84d139930f9a564ad59fd4cd9
ZY <kzou227@qq.com>
2025-11-24 22:24:49 +0800

    新增延迟消息存储

--- mahu-shared/src/main/java/cool/houge/mahu/shared/service/SharedBaseService.java
+++ mahu-shared/src/main/java/cool/houge/mahu/shared/service/SharedBaseService.java
@@ -5,9 +5,12 @@ import com.google.common.collect.ImmutableMap;
 import cool.houge.mahu.BizCodeException;
 import cool.houge.mahu.BizCodes;
 import cool.houge.mahu.StatusCodes;
+import cool.houge.mahu.entity.sys.DelayMessage;
 import cool.houge.mahu.shared.FeatureConfig;
 import cool.houge.mahu.shared.G;
+import cool.houge.mahu.shared.repository.sys.DelayMessageRepository;
 import cool.houge.mahu.shared.repository.sys.FeatureRepository;
+import io.ebean.annotation.Transactional;
 import io.helidon.service.registry.Service;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +18,8 @@ import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jspecify.annotations.NonNull;
 
 /// 基础
@@ -24,10 +29,12 @@ import org.jspecify.annotations.NonNull;
 @AllArgsConstructor
 public class SharedBaseService {
 
+    private static final Logger log = LogManager.getLogger(SharedBaseService.class);
     private final Supplier<Map<Integer, FeatureConfig>> featureConfigs =
             Suppliers.memoizeWithExpiration(this::loadFeatureMap, G.CACHE_5M_TTL);
 
     private final FeatureRepository featureRepository;
+    private final DelayMessageRepository delayMessageRepository;
 
     /// 判断是否为活跃功能
     public boolean isActiveFeature(int featureId) {
@@ -41,6 +48,12 @@ public class SharedBaseService {
             throw new BizCodeException(BizCodes.DATA_LOSS, "未找到 feature: [%s]", featureId);
         }
         return f;
+    }
+
+    /// 推送延迟消息
+    @Transactional
+    public void pushDelayMessage(DelayMessage message) {
+        delayMessageRepository.persist(message);
     }
 
     private Map<Integer, FeatureConfig> loadFeatureMap() {
