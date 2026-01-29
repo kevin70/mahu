4b2dd70cda0bffb4197b31e724a00eed14acdfff
ZY <kzou227@qq.com>
2026-01-29 16:59:37 +0800

    format

--- mahu-db/src/main/resources/db/changelog/changelog-tables/sys.delayed_task.sql
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.delayed_task.sql
@@ -2,65 +2,62 @@
 ;
 
 -- changeset kzou227@qq.com:202511232235
-create table sys.delayed_task
-(
-    id              uuid not null
-        constraint delayed_task_pk
-            primary key,
-    created_at      timestamp,
-    updated_at      timestamp,
-    feature_id      integer,
-    topic           varchar(50),
-    status          smallint,
-    delay_until     timestamp,
-    attempts        integer,
-    max_attempts    smallint,
-    lock_at         timestamp,
-    lease_seconds   smallint,
-    payload         jsonb,
-    idempotency_key varchar(128)
+CREATE TABLE sys.delayed_task (
+  id UUID NOT NULL CONSTRAINT delayed_task_pk PRIMARY KEY,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  feature_id INTEGER,
+  topic VARCHAR(50),
+  status SMALLINT,
+  delay_until TIMESTAMP,
+  attempts INTEGER,
+  max_attempts SMALLINT,
+  lock_at TIMESTAMP,
+  lease_seconds SMALLINT,
+  payload JSONB,
+  idempotency_key VARCHAR(128)
 );
 
-comment on table sys.delayed_task is '延迟任务';
+comment ON TABLE sys.delayed_task IS '延迟任务';
 
-comment on column sys.delayed_task.id is '主键';
+comment ON COLUMN sys.delayed_task.id IS '主键';
 
-comment on column sys.delayed_task.created_at is '创建时间';
+comment ON COLUMN sys.delayed_task.created_at IS '创建时间';
 
-comment on column sys.delayed_task.updated_at is '更新时间';
+comment ON COLUMN sys.delayed_task.updated_at IS '更新时间';
 
-comment on column sys.delayed_task.feature_id is '功能 ID（用于追踪统计）';
+comment ON COLUMN sys.delayed_task.feature_id IS '功能 ID（用于追踪统计）';
 
-comment on column sys.delayed_task.topic is '主题';
+comment ON COLUMN sys.delayed_task.topic IS '主题';
 
-comment on column sys.delayed_task.status is '状态
+comment ON COLUMN sys.delayed_task.status IS '状态
 - 11 待处理
 - 50 进行中
 - 88 已完成
 - 95 已归档';
 
-comment on column sys.delayed_task.delay_until is '下一次可执行时间（含重试）';
+comment ON COLUMN sys.delayed_task.delay_until IS '下一次可执行时间（含重试）';
 
-comment on column sys.delayed_task.attempts is '已尝试次数';
+comment ON COLUMN sys.delayed_task.attempts IS '已尝试次数';
 
-comment on column sys.delayed_task.max_attempts is '最大尝试次数（包括首次执行）';
+comment ON COLUMN sys.delayed_task.max_attempts IS '最大尝试次数（包括首次执行）';
 
-comment on column sys.delayed_task.lock_at is '锁定时间';
+comment ON COLUMN sys.delayed_task.lock_at IS '锁定时间';
 
-comment on column sys.delayed_task.lease_seconds is '锁租约(秒)，worker 处理任务允许的最长时间';
+comment ON COLUMN sys.delayed_task.lease_seconds IS '锁租约(秒)，worker 处理任务允许的最长时间';
 
-comment on column sys.delayed_task.payload is '消息内容存储业务所需的所有数据';
+comment ON COLUMN sys.delayed_task.payload IS '消息内容存储业务所需的所有数据';
 
-comment on column sys.delayed_task.idempotency_key is '幂等键';
+comment ON COLUMN sys.delayed_task.idempotency_key IS '幂等键';
 
-create unique index delayed_task_topic_idempotency_key_uidx
-    on sys.delayed_task (topic, idempotency_key);
+CREATE UNIQUE INDEX delayed_task_topic_idempotency_key_uidx ON sys.delayed_task (topic, idempotency_key);
 
-create index delayed_task_delay_until_idx
-    on sys.delayed_task (delay_until)
-    where (status = 11);
+CREATE INDEX delayed_task_delay_until_idx ON sys.delayed_task (delay_until)
+WHERE
+  (status = 11);
 
-create index delayed_task_lock_at_idx
-    on sys.delayed_task (lock_at)
-    where (status = 50);
+CREATE INDEX delayed_task_lock_at_idx ON sys.delayed_task (lock_at)
+WHERE
+  (status = 50);
+
 -- rollback drop table sys.delay_message;
