05bb519c1ba534ad2db9065c1c95e45cff24061d
ZY <kzou227@qq.com>
2026-03-05 10:42:24 +0800

    Rename scheduled_task_exe_log to scheduled_task_logs in database schema, SQL scripts, and entity class for consistency.

--- mahu-db/src/main/resources/db/changelog/changelog-tables/sys.scheduled_task_exe_log.sql
+++ mahu-db/src/main/resources/db/changelog/changelog-tables/sys.scheduled_task_exe_log.sql
@@ -2,8 +2,8 @@
 ;
 
 -- changeset kzou227@qq.com:202509172017
-CREATE TABLE sys.scheduled_task_exe_log (
-  id UUID NOT NULL CONSTRAINT scheduled_task_exe_log_pk PRIMARY KEY,
+CREATE TABLE sys.scheduled_task_logs (
+  id UUID NOT NULL CONSTRAINT scheduled_task_logs_pk PRIMARY KEY,
   created_at TIMESTAMP,
   task_name VARCHAR(50),
   picked_by VARCHAR(50),
@@ -14,26 +14,26 @@ CREATE TABLE sys.scheduled_task_exe_log (
   fail_cause JSONB
 );
 
-COMMENT ON TABLE sys.scheduled_task_exe_log IS '定时任务执行记录';
+COMMENT ON TABLE sys.scheduled_task_logs IS '定时任务执行记录';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.id IS '主键';
+COMMENT ON COLUMN sys.scheduled_task_logs.id IS '主键';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.created_at IS '创建时间';
+COMMENT ON COLUMN sys.scheduled_task_logs.created_at IS '创建时间';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.task_name IS '任务名称';
+COMMENT ON COLUMN sys.scheduled_task_logs.task_name IS '任务名称';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.picked_by IS '任务执行者';
+COMMENT ON COLUMN sys.scheduled_task_logs.picked_by IS '任务执行者';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.start_time IS '任务开始时间';
+COMMENT ON COLUMN sys.scheduled_task_logs.start_time IS '任务开始时间';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.done_time IS '任务完成时间';
+COMMENT ON COLUMN sys.scheduled_task_logs.done_time IS '任务完成时间';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.success IS '是否执行成功';
+COMMENT ON COLUMN sys.scheduled_task_logs.success IS '是否执行成功';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.trace_id IS '日志追踪 ID';
+COMMENT ON COLUMN sys.scheduled_task_logs.trace_id IS '日志追踪 ID';
 
-COMMENT ON COLUMN sys.scheduled_task_exe_log.fail_cause IS '失败原因';
+COMMENT ON COLUMN sys.scheduled_task_logs.fail_cause IS '失败原因';
 
-CREATE INDEX scheduled_task_exe_log_task_name_i ON sys.scheduled_task_exe_log (task_name);
+CREATE INDEX scheduled_task_logs_task_name_i ON sys.scheduled_task_logs (task_name);
 
--- rollback drop table sys.scheduled_task_exe_log;
+-- rollback drop table sys.scheduled_task_logs;
